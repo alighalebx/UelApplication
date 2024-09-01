f7796d868b08ac7cce619ac5193d325745942283
alighalebx <alighaleb2001@gmail.com>
2024-09-01 20:11:28 +0300

    some-improvement

---
+++ UelApplication/Views/HomePageView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace UelApplication.Views;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
